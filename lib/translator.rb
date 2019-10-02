89f13273c36a604ca9c949416f190861644a03e1
Jack Staffen <jack.staffen@gmail.com>
2019-10-02 23:50:30 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,10 +11,9 @@ def load_library(filepath)
   emoticons.each do |emotion, emojis|
     english << emojis[0]
     japanese << emojis[1]
-  end
-
-  japanese.each do |j_emoji|
-    new_hash[:gets_meaning] = {j_emoji}
+    japanese.each do |j_emoji|
+      new_hash[:gets_meaning] = {j_emoji}
+    end
   end
   binding.pry
   new_hash
