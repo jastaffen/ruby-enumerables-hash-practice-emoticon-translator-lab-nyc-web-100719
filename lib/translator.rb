33c0c8815e637ffe7246175c72c722ea1ee40194
Jack Staffen <jack.staffen@gmail.com>
2019-10-03 01:39:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ end
 def get_japanese_emoticon(filepath, emoticon)
   emoticons = load_library(filepath)
   emoticons['get_emoticon'].each do |english, japanese|
-    if english
+    if emoticon == english
       return japanese
     end
   end
