fad029aeac77368651b7cb9f082a7576071e9433
Jack Staffen <jack.staffen@gmail.com>
2019-10-03 01:29:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ end
 
 def get_japanese_emoticon(filepath, emoticon)
   emoticons = load_library(filepath)
-    binding.pry
+  binding.pry
   if emoticons['get_emoticon'][emojis[0]]
 
     return emojis[1]
