528552ca01db11a8702b9d4302e4bc124a513d9c
Jack Staffen <jack.staffen@gmail.com>
2019-10-03 00:57:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ require "yaml"
 
 def load_library(filepath)
   emoticons = YAML.load_file(filepath)
-  new_hash = {'get_meaning' => {}, ':get_emoticon' => {}}
+  new_hash = {'get_meaning' => {}, 'get_emoticon' => {}}
   emoji_arr = []
   emoticons.each do |emotion, emojis|
     emojis.each do |emoji|
