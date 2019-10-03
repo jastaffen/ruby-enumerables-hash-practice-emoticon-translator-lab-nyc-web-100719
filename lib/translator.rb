77dbc876b8c1e7d12005bfab8b5f1cc659e2b181
Jack Staffen <jack.staffen@gmail.com>
2019-10-03 01:21:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,22 +8,8 @@ def load_library(filepath)
   new_hash = {'get_meaning' => {}, 'get_emoticon' => {}}
   emoji_arr = []
   emoticons.each do |emotion, emojis|
-    # binding.pry
     new_hash['get_meaning'][emojis[1]] = emotion
     new_hash['get_emoticon'][emojis[0]] = emojis[1]
-    # emojis.each do |emoji|
-    #   emoji_arr << emoji
-  # end
-  #   i = 0
-  #   while i < emoji_arr.length do
-  #     #binding.pry
-  #     if (i % 2 == 0)
-  #       new_hash['get_emoticon'][emoji_arr[i]] = emoji_arr[i  1]
-  #     else
-  #       new_hash['get_meaning'][emoji_arr[i]] = emotion
-  #     end
-  #     i += 1
-  #   end
   end
   new_hash
 end
