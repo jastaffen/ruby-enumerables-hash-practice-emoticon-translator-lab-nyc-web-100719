4c7bcf661906896d816e1fac2bd4d05ad152dbb5
Jack Staffen <jack.staffen@gmail.com>
2019-10-03 00:28:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,6 +16,8 @@ def load_library(filepath)
       if emoji_arr[i] % 2 != 0
         new_hash[:get_meaning][emoji_arr[i]] = emotion
       end
+      if emoji_arr[i] % 2 == 0
+        new_hash[:get_emoticon][]
       i += 1
     end
   end
